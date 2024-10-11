187a40a9b77ba05bf6dbe1dea3545bc2f3a172f0
MajVicious <147520207+team-ganztags@users.noreply.github.com>
2024-10-11 13:33:47 +0200

    Updated Script Using $_ENV

--- src/php/mailer.php
+++ src/php/mailer.php
@@ -5,11 +5,14 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+// Import the Dotenv class from the vlucas/phpdotenv package
+use Dotenv\Dotenv;
+
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
 // Initialize Dotenv
-$dotenv = Dotenv/Dotenv::createImmutable(__DIR__);  // Use __DIR__ to reference the current directory
+$dotenv = Dotenv::createImmutable(__DIR__);  // Use __DIR__ to reference the current directory
 $dotenv->load();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -27,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host       = 'w0122a15.kasserver.com';               //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = $_ENV['SMTP_USER'];                    //SMTP username
-        $mail->Password   = $_ENV['SMTP_PASSWORD'];                     //SMTP password
+        $mail->Password   = $_ENV['SMTP_PASS'];                     //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
